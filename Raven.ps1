c45cbeba7e720deb12852aa501b2e7a6fa4ee116
William Hearn <williamhearn17@gmail.com>
2023-07-12 18:08:19 -0400

    TOOOOO LAZZZY TO TRACK CHANGES

--- Raven.ps1
+++ Raven.ps1
@@ -1,16 +1,16 @@
 param (
-    [Parameter(Mandatory=$true)]
+    [Parameter(Mandatory = $true)]
     [string]$UserPrincipalName
 )
 
-function Write-Log($message, $foregroundColor = 'Green') {
+function Write-LogMessage($message, $foregroundColor = 'Green') {
     $date = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $colorMessage = "$date - $message"
     Microsoft.PowerShell.Utility\Write-Host -ForegroundColor $foregroundColor $colorMessage
 }
 
 
-function Draw-Logo {
+function Show-Logo {
     Clear-Host
     Write-Host @"
     @                                                             
@@ -39,25 +39,28 @@ function Draw-Logo {
                 &@,@@@ ,#@ ,     %  
 
 "@
-    Write-Log "Raven v0.4"
+    Write-LogMessage "Raven v0.6"
 }
 
 
-function Prompt-User {
-    try {
-        $startDaysAgo = [int](Read-Host "Please enter the start date (as a number of days ago between 0 and 90, defaults to 90)")
-    }
-    catch {
-        Write-Log "Invalid input. Please enter a valid number." 'Red'
+function Read-UserInput {
+    $userInput = Read-Host "Please enter the start date (as a number of days ago between 0 and 90, defaults to 90)"
+    if (![int32]::TryParse($userInput, [ref]$startDaysAgo)) {
+        Write-LogMessage "Invalid input. Please enter a valid number." 'Red'
         exit
     }
 
     $directory = Read-Host "Please enter the directory to save the audit logs"
+    
+    if (!(Test-Path $directory -PathType Container)) {
+        Write-LogMessage "Invalid directory. Please enter a valid directory." 'Red'
+        exit
+    }
 
     if ([string]::IsNullOrWhiteSpace($startDaysAgo)) { $startDaysAgo = 90 }
 
     if ($startDaysAgo -lt 0 -or $startDaysAgo -gt 90) {
-        Write-Log "Start date should be between 0 and 90 days ago" 'Red'
+        Write-LogMessage "Start date should be between 0 and 90 days ago" 'Red'
         exit
     }
 
@@ -66,74 +69,90 @@ function Prompt-User {
 }
 
 
-function Set-Dates($startDaysAgo) {
+
+function Get-DateRange($startDaysAgo) {
     [DateTime]$start = [DateTime]::UtcNow.AddDays(-$startDaysAgo)
     [DateTime]$end = Get-Date
     $end = $end.AddMinutes(-$end.Minute).AddSeconds(-$end.Second).AddMilliseconds(-$end.Millisecond)
     return $start, $end
 }
 
-function Connect-Exchange {
+function Connect-ExchangeService {
     if (!(Get-ConnectionInformation | Where-Object { $_.Name -match 'ExchangeOnline' -and $_.state -eq 'Connected' })) { 
         Connect-ExchangeOnline -ShowBanner:$false
     }
     else {
-        Write-Log "Already connected to Exchange Online"
+        Write-LogMessage "Already connected to Exchange Online"
     }
 
     $config = Get-AdminAuditLogConfig
     if ($null -eq $config -or !$config.UnifiedAuditLogIngestionEnabled) {
-        Write-Log "Audit logging not enabled on tenant" 'Red'
+        Write-LogMessage "Audit logging not enabled on tenant" 'Red'
         exit
     }
 }
 
 
-function Retrieve-AuditRecords($start, $end, $outputFile) {
+function Get-AuditRecords($start, $end, $outputFile) {
     $resultSize = 5000
     $intervalMinutes = 60
     $totalIntervals = [Math]::Ceiling(($end - $start).TotalMinutes / $intervalMinutes)
-    $intervalCount = 0
-    [DateTime]$currentStart = $start
+
     [DateTime]$currentEnd = $end
-    $totalCount = 0
 
-    while ($currentStart -lt $end) {
-        $currentEnd = $currentStart.AddMinutes($intervalMinutes)
-        if ($currentEnd -gt $end) {
-            $currentEnd = $end
-        }
-
-        $sessionID = [Guid]::NewGuid().ToString() + "_" + "ExtractLogs" + (Get-Date).ToString("yyyyMMddHHmmssfff")
+    for ($intervalCount = 0; $intervalCount -lt $totalIntervals; $intervalCount++) {
+        
+        $currentStart, $currentEnd = Get-TimeInterval $start $end $intervalMinutes $intervalCount
+        $sessionID = New-SessionID
 
         do {
-            $results = Search-UnifiedAuditLog -StartDate $currentStart -EndDate $currentEnd -SessionId $sessionID -SessionCommand ReturnLargeSet -ResultSize $resultSize -UserIds $UserPrincipalName
+            $results = Search-AuditLog $currentStart $currentEnd $sessionID $resultSize $UserPrincipalName $outputFile
+        } while ($results)
 
-            if (($results | Measure-Object).Count -ne 0) {
-                $results | export-csv -Path $outputFile -Append -NoTypeInformation
-                $totalCount += $results.Count
-            }
-        }
-        while (($results | Measure-Object).Count -ne 0)
-
-        $currentStart = $currentEnd
-
-        $intervalCount++
-        $progress = [math]::Round((($intervalCount / $totalIntervals) * 100), 2)
-        Write-Progress -Activity "Retrieving audit records" -Status "$progress% Complete:" -PercentComplete $progress
-    }
-
-    if ($totalCount -eq 0) {
-        Write-Log "No audit logs were found in the specified date range." 'Red'
-    }
-    else {
-        Write-Log "Audit logs retrieval completed. Total logs found: $totalCount"
+        Show-Progress $intervalCount $totalIntervals
     }
 }
 
-# Main script
-$startDaysAgo, $outputFile = Prompt-User
-$start, $end = Set-Dates $startDaysAgo
-Draw-Logo
-Connect-Exchange
-Retrieve-AuditRecords $start $end $outputFile
+
+function Get-TimeInterval($start, $end, $intervalMinutes, $intervalCount) {
+    [DateTime]$currentStart = $start.AddMinutes($intervalCount * $intervalMinutes)
+    [DateTime]$currentEnd = $currentStart.AddMinutes($intervalMinutes)
+
+    if ($currentEnd -gt $end) {
+        $currentEnd = $end
+    }
+
+    return $currentStart, $currentEnd
+}
+
+
+function New-SessionID() {
+    return [Guid]::NewGuid().ToString() + "_" + "ExtractLogs" + (Get-Date).ToString("yyyyMMddHHmmssfff")
+}
+
+
+function Search-AuditLog($currentStart, $currentEnd, $sessionID, $resultSize, $UserPrincipalName, $outputFile) {
+    $results = Search-UnifiedAuditLog -StartDate $currentStart -EndDate $currentEnd -SessionId $sessionID -SessionCommand ReturnLargeSet -ResultSize $resultSize -UserIds $UserPrincipalName
+
+    if (($results | Measure-Object).Count -ne 0) {
+        $results | Export-Csv -Path $outputFile -Append -NoTypeInformation
+        return $results
+    }
+}
+
+
+function Show-Progress($intervalCount, $totalIntervals) {
+    $progress = [math]::Round((($intervalCount / $totalIntervals) * 100), 0)
+    Write-Progress -Activity "Retrieving audit records" -Status "$progress% Complete:" -PercentComplete $progress
+}
+
+
+function main {
+    $startDaysAgo, $outputFile = Read-UserInput
+    $start, $end = Get-DateRange $startDaysAgo
+    Show-Logo
+    Connect-ExchangeService
+    Get-AuditRecords $start $end $outputFile
+}
+
+main
